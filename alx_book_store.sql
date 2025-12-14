c7f83850e656fde51465a60a9b6b342f749d57da
richmond adusei <richmond.nana2000@gmail.com>
2025-12-14 20:00:25 +0300

    changes

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+USE alx_book_store;
+
 CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(255) NOT NULL
@@ -9,7 +13,7 @@ CREATE TABLE IF NOT EXISTS Books (
     author_id INT,
     price DECIMAL(10, 2) NOT NULL,
     published_date DATE,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 CREATE TABLE IF NOT EXISTS Customers (
